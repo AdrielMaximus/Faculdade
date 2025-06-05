0e49d34d20007616d4ada2fac05e2d06109eac64
Adriel Maximus <adrvlogs885@gmail.com>
2025-06-05 09:50:29 -0300

    eu acabo

--- global.js
+++ global.js
@@ -100,6 +100,37 @@ document.getElementById('formCadastro').addEventListener('submit', async (e) => 
   }
 });
 
+document.getElementById('formLogin').addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const senha = document.getElementById('senha').value;
+
+  try {
+    const response = await fetch('http://<IP-PUBLICO-EC2>:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, senha }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Email ou senha inválidos.');
+    }
+
+    const data = await response.json();
+    localStorage.setItem('isLoggedIn', true); // Armazena o estado de login
+    localStorage.setItem('userEmail', email); // Armazena o email do usuário
+    localStorage.setItem('usuarioId', data.id); // Armazena o ID do usuário
+    alert('Login realizado com sucesso!');
+    window.location.href = 'meus-agendamentos.html'; // Redireciona para "Meus Agendamentos"
+  } catch (error) {
+    console.error(error);
+    alert('Erro ao fazer login.');
+  }
+});
+
 document.getElementById('formAgendamento').addEventListener('submit', async function (e) {
   e.preventDefault();
 
